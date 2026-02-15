b3200f9235280da1049baf849ef0c0d30c96e2d4
DLZSY <han.deng@outlook.com>
2026-02-14 16:11:51 -0800

    Add more examples

--- cs336_basics/unicode_encodings.py
+++ cs336_basics/unicode_encodings.py
@@ -13,12 +13,24 @@ INPUT_STR = flags.DEFINE_string('input_str',
 def get_unicode_bytes(input_str: str):
   encoded_bytes = input_str.encode('utf-8')
   print(f"Encoded bytes: {encoded_bytes}")
+  for b in encoded_bytes:
+
+    print(f"Byte value: {bytes([b])}")
+  encoded_bytes_list = list(encoded_bytes)
+  print(f"Encoded bytes as list of integers: {encoded_bytes_list}")
+
+
+def decode_utf8_bytes_to_str_wrong(bytestring: bytes):
+  return "".join(
+      [bytes([b]).decode("utf-8", errors='replace') for b in bytestring])
 
 
 def main(argv):
   del argv
   input_str = INPUT_STR.value
-  get_unicode_bytes(input_str)
+  #get_unicode_bytes(input_str)
+  result = decode_utf8_bytes_to_str_wrong('牛'.encode('utf-8'))
+  print(f"Decoded result: {result}")
 
 
 if __name__ == '__main__':
